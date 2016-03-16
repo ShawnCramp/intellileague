1f3fe0bbed4b8c5b765ea0607c341a51dd0c3d2e
Shawn Cramp <cram7290@n2095-mac-26.local>
2016-03-16 17:06:40 -0400

    GET Call

--- intellileague/intellileague/LoginViewController.swift
+++ intellileague/intellileague/LoginViewController.swift
@@ -11,12 +11,18 @@ import UIKit
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var textSummonerName: UITextField!
-    @IBOutlet weak var buttonEnter: UIButton!
+    @IBOutlet weak var ipLabel: UILabel!
+
+    @IBOutlet weak var postResultLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        // Call our two REST services
+        updateIP()
+        //postDataToURL()
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,19 +37,88 @@ class LoginViewController: UIViewController {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
         
-        // For some reason this is giving me an error...
-        if buttonEnter === sender {
+        
+    }
+    
+    
+    //MARK: - REST calls
+    // This makes the GET call to httpbin.org. It simply gets the IP address and displays it on the screen.
+    func updateIP() {
+        
+        // Setup the session to make REST GET call.  Notice the URL is https NOT http!!
+        let postEndpoint: String = "http://guarded-brushlands-62127.herokuapp.com/api/builds/1/"
+        let session = NSURLSession.sharedSession()
+        let url = NSURL(string: postEndpoint)!
+        
+        // Make the POST call and handle it in a completion handler
+        session.dataTaskWithURL(url, completionHandler: { ( data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            // Make sure we get an OK response
+            guard let realResponse = response as? NSHTTPURLResponse where
+                realResponse.statusCode == 200 else {
+                    print("Not a 200 response")
+                    return
+            }
             
-            // Save Summoner Name for use in the App
-            _ = SummonerInfo()
-            SummonerInfo.summoner.name = textSummonerName.text!
-            
-            print("test")
-            
-            
+            // Read the JSON
+            do {
+                if let ipString = NSString(data:data!, encoding: NSUTF8StringEncoding) {
+                    // Print what we got from the call
+                    print(ipString)
+                    
+                    // Parse the JSON to get the IP
+                    let jsonDictionary = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+                    let origin = jsonDictionary["name"] as! String
+                    
+                    // Update the label
+                    self.performSelectorOnMainThread("updateIPLabel:", withObject: origin, waitUntilDone: false)
+                }
+            } catch {
+                print("bad things happened")
+            }
+        }).resume()
+    }
+    
+    
+    func postDataToURL() {
+        
+        // Setup the session to make REST POST call
+        let postEndpoint: String = "http://requestb.in/r4jz64r4"
+        let url = NSURL(string: postEndpoint)!
+        let session = NSURLSession.sharedSession()
+        let postParams : [String: AnyObject] = ["hello": "Hello POST world"]
+        
+        // Create the request
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "POST"
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        do {
+            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(postParams, options: NSJSONWritingOptions())
+            print(postParams)
+        } catch {
+            print("bad things happened")
         }
         
-        
+        // Make the POST call and handle it in a completion handler
+        session.dataTaskWithRequest(request, completionHandler: { ( data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            // Make sure we get an OK response
+            guard let realResponse = response as? NSHTTPURLResponse where
+                realResponse.statusCode == 200 else {
+                    print("Not a 200 response")
+                    return
+            }
+            
+            // Read the JSON
+            if let postString = NSString(data:data!, encoding: NSUTF8StringEncoding) as? String {
+                // Print what we got from the call
+                print("POST: " + postString)
+                self.performSelectorOnMainThread("updatePostLabel:", withObject: postString, waitUntilDone: false)
+            }
+            
+        }).resume()
+    }
+    
+    func updateIPLabel(text: String) {
+        self.ipLabel.text = "Build Title: " + text
     }
     
 
